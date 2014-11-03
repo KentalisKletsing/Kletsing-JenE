fa3f4df8592d7c5a0de9a2fd3ec2d758c50408a0
Joris <jorisdouven@hotmail.com>
2014-11-03 20:52:15 +0100

    Created project :smile:

---
+++ KentalisKletsing/KentalisKletsing/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KentalisKletsing.Startup))]
+namespace KentalisKletsing
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
