ce8a5cc9543c2b4865b505278c02cce4e737fbdf
EricDeRegter <ericderegter@gmail.com>
2014-11-14 21:24:51 +0100

    zinsbouw fix

--- Kletsing/Kletsing/Zinsbouw.aspx.cs
+++ Kletsing/Kletsing/Zinsbouw.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Kletsing
 {
-    public partial class Zinsopbouw : System.Web.UI.Page
+    public partial class Zinsbouw : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
