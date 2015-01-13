69ba4693fb1efc01ccb2d1a5aa49dc2f684d814a
EricDeRegter <ericderegter@gmail.com>
2015-01-13 21:34:26 +0100

    fonologie pagina's

--- Kletsing/Kletsing/FonLiqR.aspx.cs
+++ Kletsing/Kletsing/FonLiqR.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Kletsing
 {
-    public partial class FonClusters : System.Web.UI.Page
+    public partial class FonLiqR : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
