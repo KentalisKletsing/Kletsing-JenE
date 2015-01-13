4a3a6e0510a0462e4c79fa34044ddfd7b300db81
Joris <jorisdouven@hotmail.com>
2015-01-13 18:42:02 +0100

    Login werkend

--- Kletsing/Kletsing/Login.aspx.designer.cs
+++ Kletsing/Kletsing/Login.aspx.designer.cs
@@ -11,5 +11,68 @@ namespace Kletsing {
     
     
     public partial class Login {
+        
+        /// <summary>
+        /// textEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textEmail;
+        
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// textPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textPassword;
+        
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
+        /// Persist control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox Persist;
+        
+        /// <summary>
+        /// Submit1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Submit1;
+        
+        /// <summary>
+        /// Msg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Msg;
     }
 }
--- Kletsing/Kletsing/Register.aspx.cs
+++ Kletsing/Kletsing/Register.aspx.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using Kletsing.Controllers;
 
 namespace Kletsing
 {
     public partial class Register : System.Web.UI.Page
     {
+        DBController database = new DBController();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Submit1_Click(object sender, EventArgs e)
+        {
+            if (database.AddUser(TextEmail.Text, TextPassword1.Text))
+            {
+                FormsAuthentication.RedirectFromLoginPage(TextEmail.Text, false);
+                Session.Add("UserName", TextEmail.Text);
+            }
+            else
+            {
+                Msg.Text = "Er is iets mis gegaan, probeer het opnieuw.";
+            }
         }
     }
 }
