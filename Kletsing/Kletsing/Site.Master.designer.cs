fc765de71cc88ad7b9b72f6dcad7302e6558fcd9
Joris <jorisdouven@hotmail.com>
2015-03-24 23:04:30 +0100

    add wiedje wip

--- Kletsing/Kletsing/Site.Master.designer.cs
+++ Kletsing/Kletsing/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Kletsing {
     public partial class Site {
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
